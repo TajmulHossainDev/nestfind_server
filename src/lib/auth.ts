bd354e2ed49faf0b523d902b042d38c3bee932e6
TajmulHossainDev <tajmulhossain6991@gmail.com>
2026-07-16 17:12:14 +0300

    li and auth system added

---
+++ src/lib/auth.ts
@@ -0,0 +1,23 @@
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { admin } from "better-auth/plugins";
+import { getClient } from "../config/db";
+
+const client = getClient();
+const dbName = process.env.DB_NAME!;
+
+export const auth = betterAuth({
+  database: mongodbAdapter(client.db(dbName), { client }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  trustedOrigins: [process.env.CLIENT_URL || "http://localhost:3000"],
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "lax",
+      secure: false,
+      httpOnly: true,
+    },
+  },
+  plugins: [admin()],
+});
